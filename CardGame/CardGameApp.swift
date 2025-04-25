c6157088f7dd7c901bdbdd424c74ad786700c2da
Johan Toma <sgt.creeper12@gmail.com>
2025-04-25 21:19:03 +0200

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Johant on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
