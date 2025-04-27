c564a91a95b4248fd76216d13cee2a91d6f168fd
Johan Toma <sgt.creeper12@gmail.com>
2025-04-27 06:25:09 +0200

    fin: design

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -1,21 +1,65 @@
-//
-//  ContentView.swift
-//  CardGame
-//
-//  Created by Johant on 2025-04-25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.indigo)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("logo")
+                    .padding()
+                HStack {
+                    Spacer()
+                    Image("card13")
+                    Spacer()
+                    Image("card12")
+                    Spacer()
+                }
+
+                
+                Button(action: {
+                    print("button clicked")
+                }) {
+                    Image("button")
+                }
+                
+               
+                HStack {
+                    Spacer()
+                    Text("Player")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Text("CPU")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                }
+                .padding()
+                
+                HStack {
+                    Spacer()
+                    Text("0")
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                    Text("0")
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                }
+                
+                
+            }
+            
         }
-        .padding()
     }
 }
 
